91e2e9ede5bf03b7958b8e571430859c9ea26f44
Shashwat-Trivedi <shashwatrivedi2005@gmail.com>
2025-09-20 18:11:02 +0530

    added constants

---
+++ src/constants/Navbar.ts
@@ -0,0 +1,31 @@
+//NAVBAR
+
+ export const items = [
+    {
+      label: "Features",
+      bgColor: "#0D0716",
+      textColor: "#fff",
+      links: [
+        { label: "Core Features", href: "/careers", ariaLabel: "About Features" },
+      ],
+    },
+    {
+      label: "About Ayu",
+      bgColor: "#170D27",
+      textColor: "#fff",
+      links: [
+        { label: "Featured", href: "/projects", ariaLabel: "Featured Projects" },
+        { label: "Meet Ayu", href: "/case-studies", ariaLabel: "Information about Ayu" },
+      ],
+    },
+    {
+      label: "Contact",
+      bgColor: "#271E37",
+      textColor: "#fff",
+      links: [
+        { label: "Email", href: "mailto:test@example.com", ariaLabel: "Email us" },
+        { label: "Twitter", href: "https://twitter.com", ariaLabel: "Twitter" },
+        { label: "LinkedIn", href: "https://linkedin.com", ariaLabel: "LinkedIn" },
+      ],
+    },
+  ];
